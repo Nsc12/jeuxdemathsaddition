0c4ce0e4bfd55b76d53ff1fab9bc7f0785a9ac79
Nesc <103075347+Nsc12@users.noreply.github.com>
2022-07-16 22:44:43 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,66 @@
+start = str(input ("Un mini-jeux de mathématiques : addition (go pour commencer !)"))
+while start != "go":
+    start = str(input("Un mini-jeux de mathématiques : addition (go pour commencer !) "))
+    print("Bonne chance :)")
+
+n = int(input("1 + 1 = "))
+if n == 2 :
+    print("V")
+else:
+    print("X")
+n = int(input("2 + 2 = "))
+if n == 4 :
+    print("V")
+else:
+    print("X")
+n = int(input("4 + 3 = "))
+if n == 7:
+        print("V")
+else:
+        print("X")
+
+n = int(input("3 + 1 = "))
+if n == 4:
+    print("V")
+else:
+    print("X")
+n = int(input("5 + 1 = "))
+if n == 6:
+    print("V")
+else:
+    print("X")
+n = int(input("5 + 3 = "))
+if n == 8:
+    print("V")
+else:
+    print("X")
+n = int(input("5 + 2 = "))
+if n == 7 :
+    print("V")
+else:
+    print("X")
+n = int(input("6 + 3 = "))
+if n == 9:
+    print("V")
+else:
+    print("X")
+n = int(input("6 + 2 = "))
+if n == 8:
+    print("V")
+else:
+    print("X")
+n = int(input("6 + 1 = "))
+if n == 7:
+    print("V")
+else:
+    print("X")
+n = int(input("3 + 3 = "))
+if n == 6:
+    print("V")
+else:
+    print("X")
+n = int(input("4 + 3 = "))
+if n == 7:
+    print("V")
+else:
+    print("X")
